c635d35734734aa9c782df0993fe2c35054df832
taylerderden <danizlobin_2004@mail.ru>
2023-12-10 21:51:30 +0400

    Добавьте файлы проекта.

---
+++ WPF_MyProg/DbModels/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPF_MyProg;
+
+public partial class Gender
+{
+    public string Code { get; set; } = null!;
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<Client> Clients { get; set; } = new List<Client>();
+}
---
+++ WPF_MyProg/DbModels/ServicePhoto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPF_MyProg;
+
+public partial class ServicePhoto
+{
+    public int Id { get; set; }
+
+    public int ServiceId { get; set; }
+
+    public string PhotoPath { get; set; } = null!;
+
+    public virtual Service Service { get; set; } = null!;
+}
